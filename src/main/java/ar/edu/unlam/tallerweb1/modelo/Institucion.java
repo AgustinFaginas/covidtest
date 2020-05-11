1e2f2e0de63ff8f1b2d6366a9f01697ca87709b5
unknown <nicolas.orlando24@gmail.com>
2020-05-10 22:11:48 -0300

    formateo de texto prueba-ignore

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Institucion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Institucion.java
@@ -13,32 +13,37 @@ public class Institucion {
 	private String nombre;
 	private String password;
 	private Integer camas;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Integer getCamas() {
 		return camas;
 	}
+
 	public void setCamas(Integer camas) {
 		this.camas = camas;
 	}
-	
-	
 
 }
