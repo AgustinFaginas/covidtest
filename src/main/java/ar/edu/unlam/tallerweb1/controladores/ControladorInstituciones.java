4345f2941207f24238a3f77fdb80222ef4be0492
JuanPabloSaladino <juanpablosaladino@gmail.com>
2020-05-27 22:03:21 -0300

    Se agrega el RequestMapping '/registroinstitucion' en ControladorInstitucion

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorInstituciones.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorInstituciones.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unlam.tallerweb1.modelo.Institucion;
+import ar.edu.unlam.tallerweb1.modelo.Paciente;
 import ar.edu.unlam.tallerweb1.servicios.ServicioRegistro;
 
 @Controller
@@ -25,6 +26,37 @@ public class ControladorInstituciones {
 
 		model.put("msg", message);
 		return new ModelAndView("vistaDetalle", model);
+	}
+
+	@RequestMapping("/registroinstitucion")
+	public ModelAndView registrarPaciente(
+
+			@RequestParam(value = "nombre", required = false) String nombre,
+			@RequestParam(value = "email", required = false) String email,
+			@RequestParam(value = "password", required = false) String password
+
+	) {
+		Institucion institucion = new Institucion();
+
+		institucion.setNombre(nombre);
+		institucion.setEmail(email);
+		institucion.setPassword(password);
+
+		try {
+			servicioRegistro.registrarInstitucion(institucion);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		String message = "Nombre: " + nombre;
+		String message2 = "Email: " + email;
+
+		ModelMap model = new ModelMap();
+		model.put("msg", message);
+		model.put("msg2", message2);
+
+		return new ModelAndView("registroinstitucion", model);
 	}
 
 }
